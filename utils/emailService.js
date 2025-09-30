2b68708dd2c407f3cd6a3f5be5393b6cf571b444
Shawon Talukder <shawon.talukder@green-delta.com>
2025-09-30 15:43:40 +0600

    updated with .env file updated and outlook mail service used

--- utils/emailService.js
+++ utils/emailService.js
@@ -1,12 +1,17 @@
 const nodemailer = require('nodemailer');
 
-// Create transporter
+// Create transporter for Outlook/Hotmail
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp-mail.outlook.com',
+  port: 587,
+  secure: false, // true for 465, false for other ports
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  tls: {
+    ciphers: 'SSLv3'
+  }
 });
 
 // Send OTP email
